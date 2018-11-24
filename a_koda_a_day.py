31d19720dcbb61018f6a0a6f004efbe577c58194
Kelsey Wenzel <knwenzel@gmail.com>
2018-11-24 13:20:17 -0800

    added number 2 and random image from list

--- a_koda_a_day.py
+++ a_koda_a_day.py
@@ -1,21 +1,28 @@
 from twilio.rest import Client
 import json
-
+import random
 
 with open("config.json","r") as f: 
     config = json.load(f)
-print (config)
+
+with open("img_list","r") as f:
+    img_list = f.readlines()
+    img_index = random.randint(0,len(img_list)-1)
+    img_url = img_list[img_index]
 
 # Your Account Sid and Auth Token from twilio.com/console
 account_sid = config["twilio_sid"]
 auth_token = config["twilio_auth_token"]
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-    .create(
-         body ='Heres the pooper',
-         from_= config["twilio_from_phone"],
-         to = config["twilio_to_phone"]
-     )
+number_list = config["number_list"]
+for number in number_list:
+    message = client.messages \
+        .create(
+            body ='Heres the pooper',
+            from_= config["twilio_from_phone"],
+            media_url = img_url,
+            to = number
+        )
 
-print(message.sid)
+    print(message.sid)
